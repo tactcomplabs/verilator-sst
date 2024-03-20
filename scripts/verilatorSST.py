c5afbb3def78f31a73df84c1a011a0912953dc2b
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2024-03-20 15:12:38 -0500

    fix script bug when element not already installed

--- scripts/verilatorSST.py
+++ scripts/verilatorSST.py
@@ -6,16 +6,21 @@ verilog_component = {}
 
 
 def add_component(name, element_type):
+    # todo sanitize user input
     global verilog_component
     ps = subprocess.Popen(['sst-info', element_type], stdout=subprocess.PIPE)
     element_header_path = subprocess.check_output(
         ['awk', '/using file: [^ ]*/{print $NF}'], stdin=ps.stdout,).decode('utf-8')
-    print(element_header_path)
-    lib_dir_path = os.path.join(os.path.dirname(element_header_path),os.path.pardir)
-    assert os.path.exists(lib_dir_path)
+
+    src_dir_path = ''
+    if(os.path.exists(element_header_path)):
+        src_dir_path = os.path.join(os.path.dirname(element_header_path),os.path.pardir)
+    else:
+        src_dir_path = os.path.join(os.path.dirname(__file__),os.path.pardir,'src')
+
+    assert os.path.exists(src_dir_path)
     assert (not (name in verilog_component))
-    verilog_component[name] = {'lib_dir_path': lib_dir_path,
-                               'element_type': element_type}
+    verilog_component[name] = {'src_dir_path': src_dir_path, 'element_type': element_type}
 
 
 def get_component(name):
@@ -40,11 +45,11 @@ def define_macros(name, macros):
 
 
 def compileComponent(component):
-    ret_code = subprocess.call(['make', '-C', component['lib_dir_path'],
+    ret_code = subprocess.call(['make', '-C', component['src_dir_path'],
                                'clean'], stderr=subprocess.STDOUT)
     assert ret_code == 0
     ret_code = subprocess.call(
-        ['make', f'VERILOG_MACROS={component["macros_verilog"]}', '-C', component['lib_dir_path']], stderr=subprocess.STDOUT)
+        ['make', f'VERILOG_MACROS={component["macros_verilog"]}', '-C', component['src_dir_path']], stderr=subprocess.STDOUT)
     assert ret_code == 0
 
 
