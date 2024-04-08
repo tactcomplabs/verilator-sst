8ff896d96fa8964f9e3ffcdf533407b427f3bc93
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2024-04-08 13:17:05 -0500

    move model eval out of write, add delayed write feature

--- src/sstelement/verilatorSST.cc
+++ src/sstelement/verilatorSST.cc
@@ -3,7 +3,7 @@
 #endif
 
 #include "verilatorSST.h"
-
+#include <iostream>
 using namespace SST::VerilatorSST;
 
 VerilatorSST::VerilatorSST(){
@@ -13,23 +13,22 @@ VerilatorSST::VerilatorSST(){
     contextp->traceEverOn(true);
     const char* empty {};
     contextp->commandArgs(0, &empty);
-
     top = std::make_unique<VTop>(contextp.get(), "");
+
+    signalQueue = std::make_unique<std::vector<SignalQueueEntry>>();
+
     #if VL_DEBUG
     contextp->internalsDump();
     #endif
 }
 
 void VerilatorSST::readPort(std::string portName, Signal & val){
+    std::cout << portName << std::endl;
     char *name = new char[portName.length() + 1];
     strcpy(name,portName.c_str());
 
-    vpiHandle vh1 = vpi_handle_by_name(name, NULL);;
-    if (!vh1){
-        std::string errorMessage = __PRETTY_FUNCTION__;
-        errorMessage.append(" port not found");
-        throw std::runtime_error(errorMessage);
-    }
+    vpiHandle vh1 = vpi_handle_by_name(name, NULL);
+    assert(vh1);
 
     vpi_get_value(vh1, &val);
 }
@@ -39,31 +38,67 @@ void VerilatorSST::writePort(std::string portName, Signal & val){
     strcpy(name, portName.c_str());
     
     vpiHandle vh1 = vpi_handle_by_name(name, NULL);
-    if (!vh1){
-        std::string errorMessage = __PRETTY_FUNCTION__;
-        errorMessage.append(" port not found");
-        throw std::runtime_error(errorMessage);
-    }
+    assert(vh1);
 
     s_vpi_time vpi_time_s;
     vpi_time_s.type = vpiSimTime;
     vpi_time_s.high = 0;
     vpi_time_s.low = 0;
     vpi_put_value(vh1,&val,&vpi_time_s,vpiInertialDelay);
-
-    top->eval();
 }
 
-void VerilatorSST::clockTick(uint64_t t, std::string clockPort){
-    Signal clk;
-    readPort(clockPort, clk);
+void VerilatorSST::writePortAtTick(std::string portName, Signal & signal, uint64_t writeTick){
+    assert(writeTick > getCurrentTick());
 
-    uint8_t not_clk = ~clk.getUIntValue<uint8_t>();
-    uint8_t next_clk = not_clk & 1;
-    Signal nxt_clk = Signal(1, next_clk);
-    writePort(clockPort, nxt_clk);
+    SignalQueueEntry entry{portName, writeTick, signal};
 
-    contextp->timeInc(t);
+    if (signalQueue->empty()){
+        signalQueue->push_back(entry);
+        return;
+    }
+
+    auto i = 0;
+    for(auto it : *signalQueue){
+        if(it.writeTick < writeTick){
+            break;
+        }
+        i++;
+    }
+    signalQueue->insert(signalQueue->begin() + i, entry);
+}
+
+void VerilatorSST::pollSignalQueue(){
+    while(signalQueue->empty() != true){
+        if(signalQueue->back().writeTick > getCurrentTick()){
+            break;
+        }
+
+        writePort(signalQueue->back().portName, signalQueue->back().signal);
+        signalQueue->pop_back();
+    } 
+}
+
+void VerilatorSST::tick(uint64_t elapse){
+    for(uint64_t i = 0; i < elapse; i++){
+        pollSignalQueue();
+        contextp->timeInc(1);
+        top->eval();
+    }
+}
+
+void VerilatorSST::tickClockPeriod(std::string clockPort){
+    for(auto i = 0; i < 2; i++){
+        Signal clk;
+        readPort(clockPort, clk);
+
+        uint8_t not_clk = ~clk.getUIntValue<uint8_t>();
+        uint8_t next_clk_byte = not_clk & 1;
+
+        Signal next_clk(1, next_clk_byte);
+        writePort(clockPort, next_clk );
+
+        tick(1);
+    }
 }
 
 uint64_t VerilatorSST::getCurrentTick(){
--- src/sstelement/verilatorSST.h
+++ src/sstelement/verilatorSST.h
@@ -2,28 +2,39 @@
 #define _VERILATOR_SST_H_
 
 #include <string>
-#include <functional>
-#include <stdexcept>
+#include <cassert>
 #include "VTop.h"
 #include "verilated_vpi.h"
 #include "Signal.h"
 
 namespace SST::VerilatorSST {
+struct SignalQueueEntry {
+    std::string portName;
+    uint64_t writeTick;
+    Signal signal;
+};
 
 class VerilatorSST {
     private:
     std::unique_ptr<VerilatedContext> contextp;
     std::unique_ptr<VTop> top;
+    std::unique_ptr<std::vector<SignalQueueEntry>> signalQueue;
+    
+    void pollSignalQueue();
 
     public:
     VerilatorSST();
     ~VerilatorSST(){};
-    void writePort(std::string portName, Signal & val);
-    void readPort(std::string portName, Signal & val);
-    void clockTick(uint64_t add, std::string port);
-    uint64_t getCurrentTick();
-    void finish();
 
+    void writePort(std::string portName, Signal & val);
+    void writePortAtTick(std::string portName, Signal & signal, uint64_t tick);
+    void readPort(std::string portName, Signal & val);
+
+    void tick(uint64_t elapse);
+    void tickClockPeriod(std::string clockPort);
+    uint64_t getCurrentTick();
+
+    void finish();
 };
 }
 #endif
