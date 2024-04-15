23d4e305166773d873a3e0ecdafea61c2364a6f6
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2024-04-15 18:37:29 -0500

    change Uart script defaults

--- scripts/basicVerilogUartMem.py
+++ scripts/basicVerilogUartMem.py
@@ -1,5 +1,5 @@
 import verilatorSST
 
-verilatorSST.create_component("CounterComponent", "BasicVerilogUartMem")
-verilatorSST.define_macros("CounterComponent", [('ADDR_WIDTH',4),('DATA_WIDTH',4),('BAUD_PERIOD',16),('CLOCK_FREQ','1Ghz')])
-verilatorSST.finalize("CounterComponent")
+verilatorSST.create_component("UartComponent", "BasicVerilogUartMem")
+verilatorSST.define_macros("UartComponent", [('ADDR_WIDTH',9),('DATA_WIDTH',9),('BAUD_PERIOD',16),('CLOCK_FREQ','1Ghz')])
+verilatorSST.finalize("UartComponent")
