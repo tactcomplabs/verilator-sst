b2bf9cede92f6db502d7a1941650dbd3a53c7c76
jleidel <john.leidel@gmail.com>
2024-05-22 09:06:35 -0500

    filling in bits for individual port handlers

--- verilator-sst-element/BuildPortHandlerImpls.sh
+++ verilator-sst-element/BuildPortHandlerImpls.sh
@@ -20,7 +20,9 @@ for IN in $INPUTS;do
   echo "PortEvent *p = static_cast<PortEvent*>(ev);"
   echo "// handle the message"
   echo "if( p->getAtTick() != 0x00ull ){"
+  echo "writePortAtTick(\"${SIGNAME}\",p->getPacket(),p->getAtTick());"
   echo "}else{"
+  echo "writePort(\"${SIGNAME}\",p->getPacket());"
   echo "}"
   echo "delete ev;"
   echo "}"
@@ -31,9 +33,11 @@ for OUT in $OUTPUTS;do
   NOPAREN=`sed 's/.*(\(.*\))/\1/' <<< $OUT`
   SIGNAME=`echo $NOPAREN | sed "s/,/ /g" | awk '{print $1}' | sed "s/&//g"`
   echo "void VerilatorSST$Device::handle_${SIGNAME}(SST::Event* ev){"
-  echo "PortEvent *p = static_cast<PortEvent*>(ev);"
   echo "// handle the message"
+  echo "std::vector<uint8_t> Packet;"
+  echo "Packet = readPort(\"${SIGNAME}\");"
   echo "delete ev;"
+  echo "PortEvent *pe = new PortEvent(Packet);"
   echo "}"
 done;
 
