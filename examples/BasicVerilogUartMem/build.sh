f4eb649418f10824bac5a0b68cd0b32233cf1b9c
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2024-06-03 10:57:44 -0400

    remove sst config remove from build script

--- examples/BasicVerilogUartMem/build.sh
+++ examples/BasicVerilogUartMem/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -ex
 cd $(dirname $0)
-rm ~/.sst/sstsimulator.conf
-touch ~/.sst/sstsimulator.conf
-rm -rf build install
+rm -rf build
 mkdir build
-INSTALL_PREFIX=$(realpath ./install)
 cd build
 cmake ..
 make VERBOSE=1 install 2>&1 | tee make.install.log
