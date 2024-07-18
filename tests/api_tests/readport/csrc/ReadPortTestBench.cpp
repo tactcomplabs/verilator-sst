29e2449395b83938f9558eb3541291af46c6928e
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2024-07-18 11:20:27 -0500

    add more read tests

--- tests/api_tests/readport/csrc/ReadPortTestBench.cpp
+++ tests/api_tests/readport/csrc/ReadPortTestBench.cpp
@@ -7,30 +7,64 @@
 using namespace SST::VerilatorSST;
 
 void testReadPort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
-    //TODO https://github.com/tactcomplabs/verilator-sst/issues/15
     const std::vector<uint8_t> & read_port_v = dut->readPort("read_port");
+    CHECK_EQ(read_port_v.size(), 1);
     CHECK_EQ(read_port_v[0] & 1, READ_PORT_EXP);
 }
 
+void testReadBytePort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
+    const std::vector<uint8_t> & read_byte_port_v = dut->readPort("read_byte_port");
+    CHECK_EQ(read_byte_port_v.size(), 1);
+    CHECK_EQ(read_byte_port_v[0], static_cast<uint8_t>(READ_QUAD_PORT_MSB_EXP));
+}
+
+void testReadHalfPort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
+    const std::vector<uint8_t> & read_half_port_v = dut->readPort("read_half_port");
+    CHECK_EQ(read_half_port_v.size(), 2);
+    uint16_t read_half_port;
+    memcpy(&read_half_port, read_half_port_v.data(), sizeof(uint16_t));
+    CHECK_EQ(read_half_port, static_cast<uint16_t>(READ_QUAD_PORT_MSB_EXP));
+}
+
+void testReadWordPort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
+    const std::vector<uint8_t> & read_word_port_v = dut->readPort("read_word_port");
+    CHECK_EQ(read_word_port_v.size(), 4);
+    uint32_t read_word_port;
+    memcpy(&read_word_port, read_word_port_v.data(), sizeof(uint32_t));
+    CHECK_EQ(read_word_port, static_cast<uint32_t>(READ_QUAD_PORT_MSB_EXP));
+}
+
+void testReadDoublePort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
+    const std::vector<uint8_t> & read_double_port_v = dut->readPort("read_double_port");
+    CHECK_EQ(read_double_port_v.size(), 8);
+    uint64_t read_double_port;
+    memcpy(&read_double_port, read_double_port_v.data(), sizeof(uint64_t));
+    CHECK_EQ(read_double_port, static_cast<uint32_t>(READ_QUAD_PORT_MSB_EXP));
+}
+
 void testReadQuadPort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
-    const std::vector<uint8_t> & read_port_v = dut->readPort("read_quad_port");
+    SKIP_TEST; //TODO https://github.com/tactcomplabs/verilator-sst/issues/17
+    const std::vector<uint8_t> & read_quad_port_v = dut->readPort("read_quad_port");
+    CHECK_EQ(read_quad_port_v.size(), 16);
     uint64_t read_quad_port_msb;
     uint64_t read_quad_port_lsb;
-    memcpy(&read_quad_port_msb, read_port_v.data()+8, sizeof(uint64_t));
-    memcpy(&read_quad_port_lsb, read_port_v.data(), sizeof(uint64_t));
+    memcpy(&read_quad_port_msb, read_quad_port_v.data()+8, sizeof(uint64_t));
+    memcpy(&read_quad_port_lsb, read_quad_port_v.data(), sizeof(uint64_t));
     CHECK_EQ(read_quad_port_msb, READ_QUAD_PORT_MSB_EXP);
     CHECK_EQ(read_quad_port_lsb, READ_QUAD_PORT_LSB_EXP);
 }
 
 void testReadArrayPort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
-    const std::vector<uint8_t> & read_port_v = dut->readPort("read_array_port");
+    const std::vector<uint8_t> & read_array_port_v = dut->readPort("read_array_port");
+    CHECK_EQ(read_array_port_v.size(), 128);
     for(int i = 0; i < 128; i++) {
-        CHECK_EQ(read_port_v[i] & 1, i % 2);
+        CHECK_EQ(read_array_port_v[i] & 1, i % 2);
     }
 }
 
 void testReadQuadArrayPort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
     const std::vector<uint8_t> & read_quad_array_port_v = dut->readPort("read_quad_array_port");
+    CHECK_EQ(read_quad_array_port_v.size(), 16*128);
     uint64_t quad_msb;
     uint64_t quad_lsb;
     const uint64_t mask = 0xFFFFFFFFFFFFFF00;
@@ -44,17 +78,22 @@ void testReadQuadArrayPort(SST::Output * output, SST::VerilatorSST::VerilatorSST
 
 void testReadRegPort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
     const std::vector<uint8_t> & read_reg_port_v = dut->readPort("read_reg_port");
+    CHECK_EQ(read_reg_port_v.size(), 1);
     CHECK_EQ(read_reg_port_v[0],READ_PORT_EXP);
 }
 
 void testReadWirePort(SST::Output * output, SST::VerilatorSST::VerilatorSSTBase * dut){
     const std::vector<uint8_t> & read_wire_port_v = dut->readPort("read_wire_port");
+    CHECK_EQ(read_wire_port_v.size(), 1);
     CHECK_EQ(read_wire_port_v[0],READ_PORT_EXP);
 }
 
 void ReadPortTestBench::runTestSuite(SST::Cycle_t){
     testReadPort(output, dut);
-    testReadQuadPort(output,dut);
+    testReadBytePort(output, dut);
+    testReadHalfPort(output, dut);
+    testReadWordPort(output, dut);
+    testReadQuadPort(output, dut);
     testReadArrayPort(output, dut);
     testReadQuadArrayPort(output, dut);
     testReadRegPort(output, dut);
--- tests/api_tests/readport/vsrc/DUT.sv
+++ tests/api_tests/readport/vsrc/DUT.sv
@@ -1,20 +1,27 @@
 module DUT(
-    output read_port,
+    output         read_port,
+    output [7:0]   read_byte_port,
+    output [15:0]  read_half_port,
+    output [31:0]  read_word_port,
+    output [63:0]  read_double_port,
     output [127:0] read_quad_port,
-    output read_array_port [0:127],
+    output         read_array_port [0:127],
     output [127:0] read_quad_array_port [0:127],
-    output reg read_reg_port,
-    output wire read_wire_port
+    output reg     read_reg_port,
+    output wire    read_wire_port
 );
 
-assign read_port = 1'b1;
-assign read_wire_port = 1'b1;
-assign read_quad_port = 128'h0123_4567_89AB_CDEF_FEDC_BA98_7654_3210;
+assign read_port =        1'b1;
+assign read_byte_port =   8'hEF;
+assign read_half_port =   16'hCDEF;
+assign read_word_port =   32'h89AB_CDEF;
+assign read_double_port = 64'h0123_4567_89AB_CDEF;
+assign read_quad_port =   128'h0123_4567_89AB_CDEF_FEDC_BA98_7654_3210;
+assign read_wire_port =   1'b1;
 
 genvar i;
 generate
     for(i = 0; i < 128; i = i + 1) begin
-
         assign read_quad_array_port[i] = 128'h0123_4567_89AB_CD00_FEDC_BA98_7654_3200 | {{120{1'b0}},i[7:0]} | {{120{1'b0}},i[7:0]} << 64;
         assign read_array_port[i] = i[0];
     end
