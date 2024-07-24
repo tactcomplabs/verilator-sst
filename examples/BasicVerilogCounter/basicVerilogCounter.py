bb2cf04c81eed651d6e5318185c42685eb6944d2
Dawson Fox <dawsfox@udel.edu>
2024-07-24 13:31:52 -0500

    removed example and source directories

--- examples/BasicVerilogCounter/basicVerilogCounter.py
+++
@@ -1,7 +0,0 @@
-import sst
-
-counter_component = sst.Component("CounterComponent", "verilatorsst.BasicVerilogCounter")
-counter_component.addParams({
-    'STOP_WIDTH':16,
-    'STOP': 513
-})
--- examples/BigVerilogAccum/bigVerilogAccum.py
+++
@@ -1,9 +0,0 @@
-import sst
-
-accum_component = sst.Component("AccumComponent", "verilatorsst.BigVerilogAccum")
-accum_component.addParams({
-    'ACCUM_WIDTH': 88,
-    'ADD':         0x1010406080a0c0e1,
-    'ADD2':        0xf311517191b1d1f1,
-    'CLOCK_FREQ': '1Ghz'
-})
