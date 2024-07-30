0e7c9dfc647caa5c056d37b4f80abe2d5506f0b3
rkabrick <rkabrick@udel.edu>
2024-07-30 19:11:05 -0400

    Use argparse instead of sys.argv

--- tests/test_elements/verilator-test-link/sample.py
+++ tests/test_elements/verilator-test-link/sample.py
@@ -11,21 +11,11 @@
 import os
 import sys
 import sst
+import argparse
 
 WRITE_PORT = "1"
 READ_PORT = "0"
 
-testOps = [ ]
-examples = ["Counter", "Accum", "UART", "Scratchpad"]
-if (len(sys.argv) > 2 and sys.argv[1] == "-m"):
-     sub = sys.argv[2]
-     if sub not in examples:
-          raise Exception("Unknown model selected")
-     subName = "verilatorsst{}.VerilatorSST{}".format(sub, sub)
-else:
-     # Default to accum because it is a relatively robust example
-     sub = "Accum"
-     subName = "verilatorsstAccum.VerilatorSSTAccum"
 
 class PortDef:
      """ Wrapper class to make port definitions cleaner """
@@ -37,17 +27,17 @@ class PortDef:
      # portName is a string, portSize is an int (measured in bytes), portDir
      # should use READ_PORT or WRITE_PORT globals
      def addPort(self, portName, portSize, portDir):
-          tmp = portName + ":" + str(self.PortId) + ":" + str(portSize) + ":" + portDir 
+          tmp = portName + ":" + str(self.PortId) + ":" + str(portSize) + ":" + portDir
           self.PortList.append(tmp)
           self.PortNames.append(portName)
           self.PortId = self.PortId + 1
-     
+
      def getPortMap(self):
           return(self.PortList)
-     
+
      def getNumPorts(self):
           return( len(self.PortList) )
-     
+
      def getPortName(self, index):
           return( self.PortNames[index] )
 
@@ -57,11 +47,11 @@ class Test:
      """ Class for building and exporting tests """
      def __init__(self):
           self.TestOps = [ ]
-     
+
      def addTestOp(self, portName, value, tick):
           tmp = portName + ":" + str(value) + ":" + str(tick)
           self.TestOps.append(tmp)
-     
+
      def buildScratchTest(self, numCycles):
           global scratchAddrBase
           for i in range(numCycles):
@@ -83,79 +73,97 @@ class Test:
                     self.addTestOp("rdata", 44, i)
                     self.addTestOp("en", 0, i)
                self.addTestOp("clk", 0, i)
-     
+
      def getTest(self):
           return(self.TestOps)
-     
+
      def printTest(self):
           print(self.TestOps)
 
-ports = PortDef()
-if ( sub == "Counter" ):
-     ports.addPort("clk",     1, WRITE_PORT)
-     ports.addPort("reset_l", 1, WRITE_PORT)
-     ports.addPort("stop",    1, WRITE_PORT)
-     ports.addPort("done",    1, READ_PORT)
-elif ( sub == "Accum" ):
-     ports.addPort("clk",     1,  WRITE_PORT)
-     ports.addPort("reset_l", 1,  WRITE_PORT)
-     ports.addPort("en",      1,  WRITE_PORT)
-     ports.addPort("add",     8,  WRITE_PORT)
-     ports.addPort("accum",   16, READ_PORT)
-     ports.addPort("done",    1,  READ_PORT)
-elif ( sub == "UART" ):
-     ports.addPort("clk",       1,  WRITE_PORT)
-     ports.addPort("rst_l",     1,  WRITE_PORT)
-     ports.addPort("RX",        1,  WRITE_PORT)
-     ports.addPort("TX",        1,  READ_PORT)
-     ports.addPort("mem_debug", 1,  READ_PORT)
-elif ( sub == "Scratchpad" ):
-     ports.addPort("clk",   1, WRITE_PORT)
-     ports.addPort("en",    1, WRITE_PORT)
-     ports.addPort("write", 1, WRITE_PORT)
-     ports.addPort("addr",  8, WRITE_PORT)
-     ports.addPort("len",   1, WRITE_PORT)
-     ports.addPort("wdata", 8, WRITE_PORT)
-     ports.addPort("rdata", 8, READ_PORT)
-     basicScratchTest = Test()
-     basicScratchTest.buildScratchTest(10)
-     testOps = basicScratchTest.getTest()
-     print(ports.getPortMap())
-     print("Basic test for Scratchpad:")
-     print("Scratchpad base addr 0x0300.0000.0000.0000 = %d" % scratchAddrBase)
-     basicScratchTest.printTest()
+def main():
 
-tester = sst.Component("vtestLink0", "verilatortestlink.VerilatorTestLink")
-tester.addParams({
-  "verbose" : 4,
-  "clockFreq" : "1GHz",
-  "num_ports" : ports.getNumPorts(),
-  "portMap" : ports.getPortMap(),
-  "testOps" : testOps,
-  "numCycles" : 20
-})
+    testOps = []
+    examples = ["Counter", "Accum", "UART", "Scratchpad"]
+    parser = argparse.ArgumentParser(description="Sample script to run verilator SST examples")
+    parser.add_argument("-m", "--model", choices=examples, default="Accum", help="Select model from examples: Counter, Accum, UART, Scratchpad")
 
-verilatorsst = sst.Component("vsst", "verilatorcomponent.VerilatorComponent") # and verilatorsst component
-verilatorsst.addParams({
-     "numCycles" : 20
-})
-model = verilatorsst.setSubComponent("model", subName) # and verilatorsst subcomponent
-model.addParams({
-    "useVPI" : 0,
-    "clockFreq" : "2.0GHz",
-    "clockPort" : "clk"
-})
+    args = parser.parse_args()
 
-"""
-model = tester.setSubComponent("model", subName)
-model.addParams({
-  "useVPI" : 1,
-  "clockFreq" : "1GHz",
-  "clockPort" : "clk",
-})
-"""
+    if args.model not in examples:
+        raise Exception("Unknown model selected")
 
-Links = [ ]
-for i in range(ports.getNumPorts()):
-     Links.append( sst.Link( "link"+str(i) ) )
-     Links[i].connect( ( model, ports.getPortName( i ), "0ps" ), ( tester, "port"+str(i), "0ps" ) )
+    sub = args.model
+
+    subName = "verilatorsst{}.VerilatorSST{}".format(args.model, args.model)
+    ports = PortDef()
+    if ( sub == "Counter" ):
+        ports.addPort("clk",     1, WRITE_PORT)
+        ports.addPort("reset_l", 1, WRITE_PORT)
+        ports.addPort("stop",    1, WRITE_PORT)
+        ports.addPort("done",    1, READ_PORT)
+    elif ( sub == "Accum" ):
+        ports.addPort("clk",     1,  WRITE_PORT)
+        ports.addPort("reset_l", 1,  WRITE_PORT)
+        ports.addPort("en",      1,  WRITE_PORT)
+        ports.addPort("add",     8,  WRITE_PORT)
+        ports.addPort("accum",   16, READ_PORT)
+        ports.addPort("done",    1,  READ_PORT)
+    elif ( sub == "UART" ):
+        ports.addPort("clk",       1,  WRITE_PORT)
+        ports.addPort("rst_l",     1,  WRITE_PORT)
+        ports.addPort("RX",        1,  WRITE_PORT)
+        ports.addPort("TX",        1,  READ_PORT)
+        ports.addPort("mem_debug", 1,  READ_PORT)
+    elif ( sub == "Scratchpad" ):
+        ports.addPort("clk",   1, WRITE_PORT)
+        ports.addPort("en",    1, WRITE_PORT)
+        ports.addPort("write", 1, WRITE_PORT)
+        ports.addPort("addr",  8, WRITE_PORT)
+        ports.addPort("len",   1, WRITE_PORT)
+        ports.addPort("wdata", 8, WRITE_PORT)
+        ports.addPort("rdata", 8, READ_PORT)
+        basicScratchTest = Test()
+        basicScratchTest.buildScratchTest(10)
+        testOps = basicScratchTest.getTest()
+        print(ports.getPortMap())
+        print("Basic test for Scratchpad:")
+        print("Scratchpad base addr 0x0300.0000.0000.0000 = %d" % scratchAddrBase)
+        basicScratchTest.printTest()
+
+    tester = sst.Component("vtestLink0", "verilatortestlink.VerilatorTestLink")
+    tester.addParams({
+    "verbose" : 4,
+    "clockFreq" : "1GHz",
+    "num_ports" : ports.getNumPorts(),
+    "portMap" : ports.getPortMap(),
+    "testOps" : testOps,
+    "numCycles" : 20
+    })
+
+    verilatorsst = sst.Component("vsst", "verilatorcomponent.VerilatorComponent") # and verilatorsst component
+    verilatorsst.addParams({
+        "numCycles" : 20
+    })
+    model = verilatorsst.setSubComponent("model", subName) # and verilatorsst subcomponent
+    model.addParams({
+        "useVPI" : 0,
+        "clockFreq" : "2.0GHz",
+        "clockPort" : "clk"
+    })
+
+    """
+    model = tester.setSubComponent("model", subName)
+    model.addParams({
+    "useVPI" : 1,
+    "clockFreq" : "1GHz",
+    "clockPort" : "clk",
+    })
+    """
+
+    Links = [ ]
+    for i in range(ports.getNumPorts()):
+        Links.append( sst.Link( "link"+str(i) ) )
+        Links[i].connect( ( model, ports.getPortName( i ), "0ps" ), ( tester, "port"+str(i), "0ps" ) )
+
+if __name__ == "__main__":
+    main()
