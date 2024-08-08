783f5ed37bbe1ff1789ce9a222d7a5a4bbba3921
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2024-08-08 12:24:29 -0500

    create inout test

---
+++ test/pin/Pin.sv
@@ -0,0 +1,21 @@
+module pin(
+    input  clk,
+    input  direction,
+    input  [7:0] data_write,
+    output [7:0] data_read,
+    inout  [7:0] io_port
+    );
+
+reg [7:0] data_out;
+reg [7:0] data_in;
+
+assign io_port   = direction ? data_out : 'z;
+assign data_read = data_in;
+
+always @ (posedge clk)
+begin
+    data_in  <= io_port;
+    data_out <= data_write;
+end
+
+endmodule
