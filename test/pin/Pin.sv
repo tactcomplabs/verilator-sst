ba89f99f12eee32c0c236ce9648e679398776ca1
Moubarak Jeje <97465738+mjeje@users.noreply.github.com>
2024-08-11 23:18:20 -0500

    remove debug prints from Pin module

--- test/pin/Pin.sv
+++ test/pin/Pin.sv
@@ -16,51 +16,23 @@
 //     io_port = DATA
 
 module Pin(
-    input  direction,
-    input  [7:0] data_write,
-    output [7:0] data_read,
-    inout wire [7:0] io_port,
+    input wire        direction,
+    input wire [7:0]  data_write,
+    output wire [7:0] data_read,
+    inout wire [7:0]  io_port,
     input  clk
     );
 
 reg [7:0] data_out;
 reg [7:0] data_in;
-wire [7:0] moubarak;
-assign moubarak = data_out;
-assign io_port = data_out;
+
+assign io_port = direction ? data_out : 8'hzz;
 assign data_read = data_in;
 
 always @ (posedge clk)
 begin
-    data_in  = io_port;
-    data_out = data_write;
-    $display("\nverilog: tick=%t moubarak=0x%h\n",$time,    moubarak);
-    $display("\nverilog: tick=%t data_in=0x%h\n",$time,    data_in);
-    $display("\nverilog: tick=%t data_out=0x%h\n",$time,   data_out);
-    $display("\nverilog: tick=%t data_write=0x%h\n",$time, data_write);
-    $display("\nverilog: tick=%t data_read=0x%h\n",$time,  data_read);
-    $display("\nverilog: tick=%t direction=0x%h\n",$time,  direction);
-    $display("\nverilog: tick=%t io_port=0x%h\n",$time,    io_port);
-end
-
-always @ (negedge clk)
-begin
-    $display("\nverilog: tick=%t moubarak=0x%h\n",$time,    moubarak);
-    $display("\nverilog: tick=%t data_in=0x%h\n",$time,    data_in);
-    $display("\nverilog: tick=%t data_out=0x%h\n",$time,   data_out);
-    $display("\nverilog: tick=%t data_write=0x%h\n",$time, data_write);
-    $display("\nverilog: tick=%t data_read=0x%h\n",$time,  data_read);
-    $display("\nverilog: tick=%t direction=0x%h\n",$time,  direction);
-    $display("\nverilog: tick=%t io_port=0x%h\n",$time,    io_port);
-end
-initial begin
-    $display("\nverilog: tick=%t moubarak=0x%h\n",$time,    moubarak);
-    $display("\nverilog: tick=%t data_in=0x%h\n",$time,    data_in);
-    $display("\nverilog: tick=%t data_out=0x%h\n",$time,   data_out);
-    $display("\nverilog: tick=%t data_write=0x%h\n",$time, data_write);
-    $display("\nverilog: tick=%t data_read=0x%h\n",$time,  data_read);
-    $display("\nverilog: tick=%t direction=0x%h\n",$time,  direction);
-    $display("\nverilog: tick=%t io_port=0x%h\n",$time,    io_port);
+    data_in  <= io_port;
+    data_out <= data_write;
 end
 
 endmodule
