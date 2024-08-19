10e8199b43a7f7be1b7acd1359a134bc7663ecad
Dawson Fox <dawsfox@udel.edu>
2024-08-19 16:53:41 -0400

    added extra verilator options to script

--- scripts/BuildVerilatorSrc.sh
+++ scripts/BuildVerilatorSrc.sh
@@ -5,18 +5,21 @@
 # contact@tactcomplabs.com
 # See LICENSE in the top level directory for licensing details
 
+#set -ex #TODO: figure out how to get cmake to stop with build errors
 BUILDDIR=$1
 SOURCEDIR=$2
 TOP=$3
 SRC=$4
-ENABLE_INOUT_HANDLING=$5
+OPTIONS=$5
+ENABLE_INOUT_HANDLING=$6
 
-ARGS=""
 if [[ "$ENABLE_INOUT_HANDLING" == "ON" ]]; then
-    ARGS="$ARGS --pins-inout-enables"
+  echo "OPTIONS = $OPTIONS"
+  OPTIONS="$OPTIONS --pins-inout-enables"
+  echo "OPTIONS = $OPTIONS"
 fi
 
-verilator --cc --vpi --public-flat-rw $ARGS -CFLAGS "-fPIC -std=c++17" --Mdir $BUILDDIR -y $SOURCEDIR --prefix VTop --top-module $TOP $SRC
+verilator --cc --vpi --public-flat-rw $OPTIONS -CFLAGS "-fPIC -std=c++17" --Mdir $BUILDDIR -y $SOURCEDIR --prefix VTop --top-module $TOP $SRC
 cd $BUILDDIR
 make -f VTop.mk
 
